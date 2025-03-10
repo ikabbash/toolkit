a947c47ac93305d091d731d2290cab283b360bde
ikabbash <i.kabbash21@gmail.com>
2025-03-10 10:55:25 +0200

    update: Fixed init-k8s.sh script to be able to use latest version of Kubespray

--- scripts/bash/kubernetes-installer/README.md
+++ scripts/bash/kubernetes-installer/README.md
@@ -9,3 +9,21 @@
 ## Enhancements
 - [ ] Add an option to create a secret for the container image registries using `kubectl create secret generic -n ${namespace} ${secret-name} --from-file=.dockerconfigjson=/path/to/config.json --type=kubernetes.io/dockerconfigjson`
 - [ ] Add an option to create a TLS secret using `kubectl create secret -n ${namespace} tls ${secret_name} --cert=/path/to/crt --key=/path/to/key`
+- [ ] Add code to deploy worker nodes instead of having all nodes to be controlplane (update step 3)
+- [ ] Etcd is deployed as a static pod instead of the default systemd
+
+## Local DNS Issue
+- If you found your local DNS pod crashing with the following error after reboot, there's most likely an issue with the resolver configuration
+    ```
+    [FATAL] plugin/loop: Loop (169.254.25.10:42096 -> 169.254.25.10:53) detected for zone ".", see https://coredns.io/plugins/loop#troubleshooting. Query: "HINFO 8227980046591666151.5519681328841792818."
+    ```
+- You can refer to this (Github issue)[https://github.com/kubernetes-sigs/kubespray/issues/9948#issuecomment-1533876540] for more explanation and a workaround
+- TLDR: Both coredns and nodelocaldns 'fall back' to the host's DNS setting, hence its stuck in a loop where its trying to resolve nodelocaldns then host and so on
+- A way to fix it without having to use `resolvconf_mode: none` option that worked for me in my own VMWare setup is to make sure the content that's in `/run/systemd/resolve/resolv.conf` is in `/etc/resolv.conf` because that's what broke the loop
+    - [Reference one](https://coredns.io/plugins/loop/#troubleshooting-loops-in-kubernetes-clusters)
+    - [Reference two](https://stackoverflow.com/questions/54466359/coredns-crashloopbackoff-in-kubernetes)
+    - If you're using VMWare with an automatic bridged network, you'll need a static IP address. Refer to this [documentation](https://ubuntu.com/server/docs/configuring-networks#static-ip-address-assignment)
+
+## References
+- https://github.com/kubernetes-sigs/kubespray/blob/master/docs/getting_started/getting-started.md
+- https://github.com/kubernetes-sigs/kubespray/blob/master/inventory/sample/inventory.ini
--- scripts/bash/kubernetes-installer/init-k8s.sh
+++ scripts/bash/kubernetes-installer/init-k8s.sh
@@ -12,6 +12,12 @@ cat << 'EOF'
 +---------------------------------------------------------+
 EOF
 
+# Get the latest tag of any Github repo
+get_latest_release () {
+  local repo_path=$1
+  git ls-remote --tags https://github.com/"${repo_path}".git | awk -F'/' '{print $NF}' | grep -v '{}' | grep -E '^[vV]?[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1
+}
+
 # Define colors
 RED='\033[0;31m'
 YELLOW='\033[0;33m'
@@ -19,46 +25,14 @@ BLUE='\033[0;34m'
 BOLD='\033[1m'
 RESET='\033[0m'
 
-# Get the latest tag of any Github repo
-get_latest_release () {
-  local repo_path=$1
-  git ls-remote --tags https://github.com/"${repo_path}".git | awk -F'/' '{print $NF}' | grep -v '{}' | grep -E '^[vV]?[0-9]+\.[0-9]+\.[0-9]+$' | sort -V | tail -n 1
-}
-
-# For Kubespray setup
-install_kubernetes() {
-  echo -e "[defaults]\nroles_path = /usr/local/src/kubespray/roles" > ~/.ansible.cfg
-  # Enable automatic renewal for cluster certs
-  sed -i "s/auto_renew_certificates\: false/auto_renew_certificates: true/g" "${KUBESPRAY_SRC_DIR}"/inventory/mycluster/group_vars/k8s_cluster/k8s-cluster.yml
-  # Deploy kubernetes cluster using Ansible Playbook
-  ansible-playbook -i "${KUBESPRAY_SRC_DIR}"/inventory/mycluster/hosts.yml -u ${USER} --become --become-user=root "${KUBESPRAY_SRC_DIR}"/cluster.yml
-  echo -e "${BLUE}${BOLD}Cluster has been installed successfully${RESET}" && sleep 5
-  # Test Kubernetes cluster installation
-  echo -e "${BLUE}Validating the installed kubernetes cluster..${RESET}"
-  sudo su -c "kubectl version"
-  sudo su -c "kubectl get nodes -o wide"
-  sudo su -c "kubectl get all -A"
-  for CLUSTER_NODE_IP in "${CLUSTER_NODES_IPS[@]}"
-  do
-    ssh -t "${CLUSTER_NODE_IP}" "sudo su -c 'curl -k https://"${CLUSTER_NODE_IP}":6443/readyz?verbose'"
-  done
-}
-
-# For Nginx ingress controller installation
-install_helm() {
-  cd /tmp
-  curl -LO https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
-  tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
-  sudo mv linux-amd64/helm /usr/local/bin/helm
-  cd
-}
-
+# Env vars
 BASE_DIR=$(readlink -f $(dirname ${0}))
 SSH_PORT="22"
-KUBESPRAY_SRC_DIR="/usr/local/src/kubespray"
-PYTHON_ENV_DIR="${KUBESPRAY_SRC_DIR}/python-venv"
 CLUSTER_NODES_IPS=(192.168.1.101 192.168.1.104) # CHANGE THIS
+KUBESPRAY_SRC_DIR="/usr/local/src/kubespray"
+KUBESPRAY_INV_FILE="${KUBESPRAY_SRC_DIR}"/inventory/mycluster/hosts.ini
 KUBESPRAY_VERSION=$(get_latest_release "kubernetes-sigs/kubespray")
+PYTHON_ENV_DIR="${KUBESPRAY_SRC_DIR}/python-venv"
 HELM_VERSION=$(get_latest_release "helm/helm")
 
 # Step 1: Enable passwordless sudo and ensure connection on target servers with same username (a prompt for password might appear)
@@ -88,7 +62,7 @@ init_connections () {
   ssh-add ~/.ssh/admin-k8s
   for CLUSTER_NODE_IP in ${CLUSTER_NODES_IPS[@]}
   do
-    ssh-keyscan -H "${CLUSTER_NODE_IP}" >>  ~/.ssh/known_hosts
+    ssh-keyscan -H "${CLUSTER_NODE_IP}" >> ~/.ssh/known_hosts
     # Enable passwordless sudo on servers
     ssh -t ${USER}@${CLUSTER_NODE_IP} "if sudo cat /etc/sudoers | grep -q "NOPASSWD"; then \
     echo "user ${USER} has passwordless sudo privilges on the server with IP ${CLUSTER_NODE_IP}"; else \
@@ -110,10 +84,6 @@ download_dependencies () {
     sudo swapoff -a && \
     sudo ufw disable"
   done
-}
-
-# Step 3: Prepare Kubespray and install Kubernetes
-setup_kubespray () {
   # Check if repo is already cloned or not
   if [ -d "${KUBESPRAY_SRC_DIR}" ]; then
     echo -e "${BLUE}Kubespray already exists, proceeding with next step${RESET}"
@@ -126,27 +96,61 @@ setup_kubespray () {
     cd ..
     sudo chown -R ${USER}:${USER} "${KUBESPRAY_SRC_DIR}"
   fi
-  # Download Kubespray dependencies
-  local inventory_builder="${KUBESPRAY_SRC_DIR}"/contrib/inventory_builder
   python3 -m venv "${PYTHON_ENV_DIR}" && source "${PYTHON_ENV_DIR}"/bin/activate
-  pip install -r "${KUBESPRAY_SRC_DIR}"/requirements.txt -r "${inventory_builder}"/requirements.txt
+  pip install -r "${KUBESPRAY_SRC_DIR}"/requirements.txt
+}
+
+# Step 3: Prepare Kubespray
+setup_kubespray () {
   # Create cluster directory
   cp -rfp "${KUBESPRAY_SRC_DIR}"/inventory/sample "${KUBESPRAY_SRC_DIR}"/inventory/mycluster
   local nodes_count=$(echo "${#CLUSTER_NODES_IPS[@]}")
-  # Prepare Ansible Inventory file using inventory builder python script
-  echo -e "${BLUE}Preparing the inventory file for ${nodes_count} nodes${RESET}"
-  KUBE_MASTERS="${nodes_count}" \
-  CONFIG_FILE="${KUBESPRAY_SRC_DIR}"/inventory/mycluster/hosts.yml \
-  python3 "${inventory_builder}"/inventory.py "${CLUSTER_NODES_IPS[@]}"
-  install_kubernetes
+  # Prepare Ansible inventory file
+  echo -e "${BLUE}Preparing the inventory file for ${nodes_count} control plane nodes${RESET}"
+  # [kube_control_plane] section
+  echo "[kube_control_plane]" >> "${KUBESPRAY_INV_FILE}"
+  counter=1
+  for ip in "${CLUSTER_NODES_IPS[@]}"; do
+      echo -e "node${counter} ansible_host=${ip} ip=${ip} etcd_member_name=etcd${counter}" >> "${KUBESPRAY_INV_FILE}"
+      ((counter++))
+  done
+  echo -e "[etcd:children]\nkube_control_plane\n" >> "${KUBESPRAY_INV_FILE}"
+  # [kube_node] section
+  echo "[kube_node]" >> "${KUBESPRAY_INV_FILE}"
+  counter=1
+  for ip in "${CLUSTER_NODES_IPS[@]}"; do
+      echo -e "node${counter} ansible_host=${ip} ip=${ip}" >> "${KUBESPRAY_INV_FILE}"
+      ((counter++))
+  done
+  echo -e "[defaults]\nroles_path = /usr/local/src/kubespray/roles" > ~/.ansible.cfg
+}
+
+# Step 5: Install Kubernetes
+install_kubernetes () {
+  # Install kubernetes cluster using Ansible Playbook
+  ansible-playbook -i "${KUBESPRAY_INV_FILE}" -u ${USER} --become --become-user=root "${KUBESPRAY_SRC_DIR}"/cluster.yml
+  echo -e "${BLUE}${BOLD}Cluster has been installed${RESET}" && sleep 5
+  # Test Kubernetes cluster installation
+  echo -e "${BLUE}Validating the installed kubernetes cluster..${RESET}"
+  sudo su -c "kubectl version"
+  sudo su -c "kubectl get nodes -o wide"
+  sudo su -c "kubectl get all -A"
+  for CLUSTER_NODE_IP in "${CLUSTER_NODES_IPS[@]}"
+  do
+    ssh -t "${CLUSTER_NODE_IP}" "sudo su -c 'curl -k https://"${CLUSTER_NODE_IP}":6443/readyz?verbose'"
+  done
   # Exit Python venv
   deactivate
 }
 
-# Step 4: Install latest version of Helm and Nginx ingress controller
+# Step 6: Install latest version of Helm and Nginx ingress controller
 install_ingress_controller () {
   # Downloads latest version of Helm
-  install_helm
+  cd /tmp
+  curl -LO https://get.helm.sh/helm-${HELM_VERSION}-linux-amd64.tar.gz
+  tar -zxvf helm-${HELM_VERSION}-linux-amd64.tar.gz
+  sudo mv linux-amd64/helm /usr/local/bin/helm
+  cd
   cat << EOF > nginx-controller-values.yml
 controller:
   kind: daemonset
@@ -175,15 +179,17 @@ EOF
 
 # Main function
 main() {
-  echo -e "\n${BLUE}${BOLD}█▒▒▒▒ INIT CONNECTIONS ▒▒▒▒█${RESET}\n"
+  echo -e "\n${BLUE}${BOLD}█▒▒▒▒▒ INIT CONNECTIONS ▒▒▒▒▒█${RESET}\n"
   init_connections
-  echo -e "\n${BLUE}${BOLD}██▒▒▒ DOWNLOADING DEPENDENCIES ▒▒▒██${RESET}\n"
+  echo -e "\n${BLUE}${BOLD}██▒▒▒▒ DOWNLOADING DEPENDENCIES ▒▒▒▒██${RESET}\n"
   download_dependencies
-  echo -e "\n${BLUE}${BOLD}███▒▒ SETTING UP KUBESPRAY ▒▒███${RESET}\n"
+  echo -e "\n${BLUE}${BOLD}███▒▒▒ SETTING UP KUBESPRAY ▒▒▒███${RESET}\n"
   setup_kubespray
-  echo -e "\n${BLUE}${BOLD}████▒ INSTALLING KUBERNETES ▒████${RESET}\n"
+  echo -e "\n${BLUE}${BOLD}████▒▒ INSTALLING KUBERNETES ▒▒████${RESET}\n"
+  install_kubernetes
+  echo -e "\n${BLUE}${BOLD}█████▒ INSTALLING INGESS CONTROLLER ▒█████${RESET}\n"
   install_ingress_controller
-  echo -e "\n${BLUE}${BOLD}█████ KUBERNETES HAS BEEN INSTALLED SUCCESSFULLY █████${RESET}\n"
+  echo -e "\n${BLUE}${BOLD}██████ KUBERNETES HAS BEEN INSTALLED SUCCESSFULLY ██████${RESET}\n"
 }
 
 main
